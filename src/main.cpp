79aa28d572f521979fc7a752e80fff22d16ae738
javo73 <javiermoro.ar@gmail.com>
2020-03-16 20:51:06 -0300

    before triying new lib

--- src/main.cpp
+++ src/main.cpp
@@ -5,51 +5,28 @@
 //Asyc Web Server
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-float temp = 00.00;
-float hum = 00.00;
+
 //DHT22 SENSOR
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
-#define TOPSENSOR_PIN  39
+#include <DHT_U.h>
+
+//DHT22 Temperature & Humidity Sensor
+#define DHTPIN  14
 #define DHTTYPE    DHT22
-DHT topSensor(TOPSENSOR_PIN,DHTTYPE);
+
+DHT_Unified dht(DHTPIN,DHTTYPE);
+
+float temp = 00.00;
+float hum = 00.00;
 
 unsigned long startMillis;
 unsigned long currentMillis;
 unsigned long dhtpreviousMillis = 0;
-const unsigned long period = 5000; //5sec period
+const unsigned long period = 2000; //2sec period
 
-//DHT22 Temperature & Humidity Sensor
-String readDHTTemperature() {
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  // Read temperature as Celsius (the default)
-  float temp = topSensor.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  //float t = dht.readTemperature(true);
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(temp)) {    
-    Serial.println("Failed to read from DHT sensor!");
-    return "--";
-  }
-  else {
-    Serial.println(temp);
-    return String(temp);
-  }
-}
 
-String readDHTHumidity() {
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float hum = topSensor.readHumidity();
-  Serial.println(hum);
-  if (isnan(hum)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return "--";
-  }
-  else {
-    Serial.println(hum);
-    return String(hum);
-  }
-}
+
 
 //Light Variable Setup (includes relay pins, and global variables) 
 int lrelay1_Pin = 25;
@@ -74,6 +51,37 @@ const char* ssid     = "Cuna de la ciencia";
 const char* password = "enricofermi";
 // Set web server port number to 80
 AsyncWebServer server(80);
+
+/*String readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  float temp = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(temp)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(temp);
+    return String(temp);
+  }
+}
+
+String readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float hum = DHT.readHumidity();
+  Serial.println(hum);
+  if (isnan(hum)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(hum);
+    return String(hum);
+  }
+}*/
 
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
@@ -239,13 +247,14 @@ void setup(){
   // put your setup code here, to run once:
   Serial.begin(9600);
   startMillis = millis();
+  dht.begin();
   //Setting relays up
   pinMode(lrelay1_Pin, OUTPUT);
   pinMode(lrelay2_Pin, OUTPUT);
 
   setupWiFi();
   setupDateTime();
-  topSensor.begin();
+  
 
 // Print local IP address and start web server
   Serial.println("");
@@ -270,32 +279,51 @@ void setup(){
   String h2 = DateTime.format("%T");
   Serial.println(h);
   Serial.println(h2);
+  //DHT22
+  Serial.println(F("DHT22 Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  // Set delay between sensor readings based on sensor details.
 }
 void loop(){
   // put your main code here, to run repeatedly:
   unsigned long dhtMillis = millis();
   int relay_state;
-  relay_state = checkTime();
-  changeRelayState(relay_state);
+  //relay_state = checkTime();
+  //changeRelayState(relay_state);
   if (dhtMillis - dhtpreviousMillis >= period ) {
     dhtpreviousMillis = dhtMillis;
-    float newT = topSensor.readTemperature();
-      if(isnan(newT)) {
-        Serial.println("Failed to read from DHT sensor!");
-      }
-        else {
-          temp = newT;
-          Serial.println(temp);
-        }
-  
-    float newH = topSensor.readHumidity();
-        if(isnan(newH)) {
-          Serial.println("Failed to read from DHT sensor!");
-        }
-          else {
-            hum = newH;
-            Serial.println(hum);
-          }
+     sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
+    }
+    else {
+      Serial.print(F("Temperature: "));
+      Serial.print(event.temperature);
+      Serial.println(F("°C"));
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else {
+      Serial.print(F("Humidity: "));
+      Serial.print(event.relative_humidity);
+      Serial.println(F("%"));
+    }
   }
+  
 }
 
